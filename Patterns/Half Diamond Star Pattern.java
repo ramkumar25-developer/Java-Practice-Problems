b879dfcdd2992d6ae2cfccfe23d0e58dc065dbaa
Ramkumar R <ramkumardev2125@gmail.com>
2026-02-09 10:44:00 +0530

    Add Half Diamond Star Pattern Java program
    Implement a Java program to print a half diamond star pattern based on user input.

---
+++ Patterns/Half Diamond Star Pattern.java
@@ -0,0 +1,34 @@
+/*Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+Example :
+Input : 5
+Output :
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+*/
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
