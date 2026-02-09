366ddfea804c10e0bb70fd87bbc0abd5cfc30013
Ramkumar R <ramkumardev2125@gmail.com>
2026-02-09 11:38:17 +0530

    Remove unused variable 'num' from main method
    Removed unused variable 'num' from the main method.

--- Patterns/Increasing Letter Triangle Pattern.java
+++ Patterns/Increasing Letter Triangle Pattern.java
@@ -14,7 +14,6 @@ class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int num = 0;
         for(int i=0;i<n;i++){
             for(char j='A';j<='A'+i;j++){
                 System.out.print(j);
