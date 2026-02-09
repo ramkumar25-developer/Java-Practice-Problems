f00a38c7e7ea99036cf348c5376719c984fe1e46
Ramkumar R <ramkumardev2125@gmail.com>
2026-02-09 11:37:04 +0530

    Remove unused variable 'num' in Alpha-Hill Pattern
    Removed unused variable 'num' from main method.

--- Patterns/Alpha-Hill Pattern.java
+++ Patterns/Alpha-Hill Pattern.java
@@ -14,7 +14,6 @@ class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int num = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<n-1-i;j++){
                 System.out.print(" ");
