e6a61901b6c43b48fad7b991f65750ddbe861cd4
Ramkumar R <ramkumardev2125@gmail.com>
2026-02-09 11:07:28 +0530

    Add Increasing Letter Triangle Pattern program
    Implement a program to print an increasing letter triangle pattern based on user input.

---
+++ Patterns/Increasing Letter Triangle Pattern.java
@@ -0,0 +1,26 @@
+/*Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+Example :
+Input : 5
+Output :
+A
+AB
+ABC
+ABCD
+ABCDE
+
+*/
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int num = 0;
+        for(int i=0;i<n;i++){
+            for(char j='A';j<='A'+i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+    }
+}
