76ba2eb3c137f8cb5ab53ec47e35d9206bfa1848
Ramkumar R <ramkumardev2125@gmail.com>
2026-02-09 09:39:44 +0530

    Add Right-Angled Number Pyramid pattern II program
    Implement a program to print a right-angled number pyramid based on user input.

---
+++ Patterns/Right-Angled Number Pyramid - II.java
@@ -0,0 +1,23 @@
+/*Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+Example :
+Input : 5
+Output :
+1
+22
+333
+4444
+55555
+*/
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
