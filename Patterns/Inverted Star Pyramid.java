d17866e0f6bd3a249bec5c9a244684430af47c34
Ramkumar R <ramkumardev2125@gmail.com>
2026-02-09 10:38:29 +0530

    Optimised easy understandable logic

--- Patterns/Inverted Star Pyramid.java
+++ Patterns/Inverted Star Pyramid.java
@@ -15,7 +15,7 @@ class Main{
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for(int i=n-1;i>=0;i--){
-            for(int j=n-1;j>=i;j--){
+            for(int j=n-1;j>i;j--){
                 System.out.print(" ");
             }
             for(int j=2*i+1;j>0;j--){
