05494d46e343e4bd8ffd4f61c9f10075cba96ccc
Ramkumar R <ramkumardev2125@gmail.com>
2026-02-09 12:20:42 +0530

    Add symmetric butterfly pattern generator
    Implement a symmetric butterfly pattern based on user input.

---
+++ Patterns/Symmetric-Butterfly Pattern.java
@@ -0,0 +1,46 @@
+/*Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+Example :
+Input : 5
+Output :
+*        *
+**      **
+***    ***
+****  ****
+**********
+****  ****
+***    ***
+**      **
+*        *
+
+*/
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<(n*2)-(i*2);j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n-1;i>0;i--){
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<(n*2)-(i*2);j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
