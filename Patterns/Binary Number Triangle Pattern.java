b6c66bd1678a222ce5f933379f049eb90f4c6c0e
Ramkumar R <ramkumardev2125@gmail.com>
2026-02-09 10:48:39 +0530

    Add binary number triangle pattern program
    Implement a Java program to print a binary number triangle pattern based on user input.

---
+++ Patterns/Binary Number Triangle Pattern.java
@@ -0,0 +1,30 @@
+/*Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+Example :
+Input : 5
+Output :
+1 
+0 1 
+1 0 1 
+0 1 0 1 
+1 0 1 0 1
+
+*/
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                if((i+j)%2==0){
+                    System.out.print("1");
+                }
+                else{
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
