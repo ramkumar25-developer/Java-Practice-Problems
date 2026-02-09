37548cd0587d0cea393ced2cec1d9a80e8483b10
Ramkumar R <ramkumardev2125@gmail.com>
2026-02-09 12:28:39 +0530

    Add Number Pattern generator in Java
    Implement a program to print a number pattern based on input integer.

---
+++ Patterns/The Number Pattern.java
@@ -0,0 +1,29 @@
+/*Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+Example :
+Input : 5
+Output :
+5 5 5 5 5 5 5 5 5 
+5 4 4 4 4 4 4 4 5 
+5 4 3 3 3 3 3 4 5 
+5 4 3 2 2 2 3 4 5 
+5 4 3 2 1 2 3 4 5 
+5 4 3 2 2 2 3 4 5 
+5 4 3 3 3 3 3 4 5 
+5 4 4 4 4 4 4 4 5 
+5 5 5 5 5 5 5 5 5
+
+*/
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<2*n-1;i++){
+            for(int j=0;j<2*n-1;j++){
+                int distance = Math.min(Math.min(i,j),Math.min((2*n-2)-i,(2*n-2)-j));
+                System.out.print(n-distance);
+            }
+            System.out.println();
+        }
+    }
+}
