9ad9199bc348e8528a829ea8eb87e9f5321cd5ad
Ramkumar R <ramkumardev2125@gmail.com>
2026-02-09 11:34:51 +0530

    Add Alpha-Hill Pattern Java implementation
    Implement pattern printing for given integer n.

---
+++ Patterns/Alpha-Hill Pattern.java
@@ -0,0 +1,34 @@
+/*Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+Example :
+Input : 5
+Output :
+    A
+   ABA
+  ABCBA
+ ABCDCBA
+ABCDEDCBA
+
+*/
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int num = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-1-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<2*i+1;j++){
+                if(j<=i) {
+                    System.out.print((char) ('A' + j));
+                }
+                else{
+                    System.out.print((char) ('A' + (j%i) ));
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
