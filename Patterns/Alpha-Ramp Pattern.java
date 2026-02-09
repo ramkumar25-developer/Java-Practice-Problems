2505b79106f3b7283ab7cb12de9bd63d95d2b8b1
Ramkumar R <ramkumardev2125@gmail.com>
2026-02-09 11:16:50 +0530

    Add Alpha-Ramp pattern generator in Java
    This Java program generates a pattern of letters based on the input integer n. For each line, it prints the corresponding letter repeated according to the line number.

---
+++ Patterns/Alpha-Ramp Pattern.java
@@ -0,0 +1,27 @@
+/*Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+Example :
+Input : 5
+Output :
+A
+BB
+CCC
+DDDD
+EEEEE
+
+*/
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int num = 0;
+        for(int i=0;i<n;i++){
+            char ch = (char) ('A'+i);
+            for(char j=0;j<=i;j++){
+                System.out.print(ch);
+            }
+            System.out.println();
+        }
+
+    }
+}
