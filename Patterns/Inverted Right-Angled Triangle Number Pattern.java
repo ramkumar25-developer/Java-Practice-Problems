6dd73244746db554938e578438e7a23ca5814910
Ramkumar R <ramkumardev2125@gmail.com>
2026-02-09 09:49:02 +0530

    Add inverted right-angled triangle number pattern
    Implement a Java program to print an inverted right-angled triangle number pattern based on user input.

---
+++ Patterns/Inverted Right-Angled Triangle Number Pattern.java
@@ -0,0 +1,24 @@
+/*Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+Example :
+Input : 5
+Output :
+12345
+1234
+123
+12
+1
+
+*/
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
