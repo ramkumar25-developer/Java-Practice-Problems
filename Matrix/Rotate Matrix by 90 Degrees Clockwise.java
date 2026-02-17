aa866f69ecdd545b2d30950be778ca7fffa96959
Ramkumar R <ramkumardev2125@gmail.com>
2026-02-17 19:34:21 +0530

    Create Rotate Matrix by 90 Degrees Clockwise.java

---
+++ Matrix/Rotate Matrix by 90 Degrees Clockwise.java
@@ -0,0 +1,49 @@
+/*Given an n × n matrix, rotate the matrix by 90 degrees clockwise.
+You must rotate the matrix in-place, meaning you cannot use another matrix.
+Example :
+Input : 3x3
+1 2 3
+4 5 6
+7 8 9
+Output :
+7 4 1
+8 5 2
+9 6 3 */
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int arr[][] = new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=i;j<m;j++){
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+        for(int i=0;i<n;i++){
+            int left = 0,right = m-1;
+            while(left<right){
+                int temp = arr[i][left];
+                arr[i][left] = arr[i][right];
+                arr[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
